32774c7792cdf60250fd6ea1bbdd16571f0065b2
Tauheed Elahee <hobbies@tauheed-elahee.com>
2021-01-14 21:43:53 -0500

    Copy past to attepmt calling qmlengine from rust.

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,13 @@ fn main() {
             "src/qml/main.qml" as "main.qml",
          }
     );
+    
+    ressources();
+    
+    // QML Engine
+    let mut engine = QmlEngine::new();
+    engine.add_import_path(QString::from("qrc:/qml/"));
+    engine.load_file(QString::from("qrc:/qml/main.qml"));
 
+    engine.exec();
 }
